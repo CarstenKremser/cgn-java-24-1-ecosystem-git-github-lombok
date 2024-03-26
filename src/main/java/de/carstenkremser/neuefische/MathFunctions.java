03d29d773b00a7b9a0db47a8f7f73ac5def93e2c
Carsten Kremser <github@carstenkremser.de>
2024-03-26 11:38:21 +0100

    Add function factorial

--- src/main/java/de/carstenkremser/neuefische/MathFunctions.java
+++ src/main/java/de/carstenkremser/neuefische/MathFunctions.java
@@ -2,4 +2,11 @@ package de.carstenkremser.neuefische;
 
 public class MathFunctions {
 
+    long factorial(long n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
+        }
+    }
 }
