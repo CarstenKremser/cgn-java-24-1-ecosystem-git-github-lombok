36e5e280c58dad9365177895ff0f572b6054f66c
Carsten Kremser <github@carstenkremser.de>
2024-03-26 11:44:35 +0100

    Call function blahblubb in main

--- src/main/java/de/carstenkremser/neuefische/Main.java
+++ src/main/java/de/carstenkremser/neuefische/Main.java
@@ -2,10 +2,14 @@ package de.carstenkremser.neuefische;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        int randomInt = (int) Math.round(Math.random()*10);
+        blahblubb(randomInt);
     }
 
-    void blahblubb(int n) {
+    static void blahblubb(int n) {
         if (n%2==0) {
             System.out.println("blah");
         } else {
