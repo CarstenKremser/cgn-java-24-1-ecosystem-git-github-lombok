f115fe20075c02891eaee44a789f81bc81790490
Carsten Kremser <github@carstenkremser.de>
2024-03-26 11:41:45 +0100

    Add function blahblubb

--- src/main/java/de/carstenkremser/neuefische/Main.java
+++ src/main/java/de/carstenkremser/neuefische/Main.java
@@ -4,4 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    void blahblubb(int n) {
+        if (n%2==0) {
+            System.out.println("blah");
+        } else {
+            System.out.println("blubb");
+        }
+
+    }
 }
+
